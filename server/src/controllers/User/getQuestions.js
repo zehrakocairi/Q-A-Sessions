874dc581a91c476c6fe110b9362121280f6da128
zehra <zehrakocairi@gmail.com>
2024-03-23 12:32:58 +0100

    minor fixes and changes

--- server/src/controllers/User/getQuestions.js
+++ server/src/controllers/User/getQuestions.js
@@ -6,17 +6,13 @@ const getUserQuestions = async (req, res) => {
   try {
     const allQuestions = await Question.find();
 
-    const userQuestions = allQuestions.filter(
-      (question) => question.user_id.toString() === userId
-    );
+    const userQuestions = allQuestions.filter((question) => question.user_id.toString() === userId);
     res.status(200).json({
       success: true,
       result: userQuestions,
     });
   } catch (error) {
-    res
-      .status(500)
-      .json({ success: false, error: "Failed to fetch user questions" });
+    res.status(500).json({ success: false, error: "Failed to fetch user questions" });
   }
 };
 
