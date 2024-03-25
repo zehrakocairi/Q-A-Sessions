2571ca71c1d888f2965360435738c9138633aca9
zehra <zehrakocairi@gmail.com>
2024-03-25 14:53:31 +0100

    refactor completed

--- client/src/pages/PostQuestionPage/PostQuestionPage.jsx
+++ client/src/pages/PostQuestionPage/PostQuestionPage.jsx
@@ -1,10 +1,9 @@
 import "./PostQuestionPage.css";
 import React, { useEffect, useState } from "react";
-import Input from "../../components/Input";
 import useFetch from "../../hooks/useFetch";
 import MultiChoiceModules from "../../components/MultipleChoiceModules/MultipleChoiceModules.jsx";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext.js";
+import { useAuth } from "../../contexts/AuthContext.jsx";
 
 const PostQuestionPage = () => {
   const navigate = useNavigate();
@@ -22,17 +21,14 @@ const PostQuestionPage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [selectedModules, setSelectedModules] = useState([]);
-  const { isLoading, error, performFetch, cancelFetch } = useFetch(
-    "/questions/create",
-    (response) => {
-      // Extract the question ID from the response
-      const questionId = response.question._id;
-      // Navigate to the question's page
-      navigate(`/questions/${questionId}`);
+  const { isLoading, error, performFetch, cancelFetch } = useFetch("/questions/create", (response) => {
+    // Extract the question ID from the response
+    const questionId = response.question._id;
+    // Navigate to the question's page
+    navigate(`/questions/${questionId}`);
 
-      alert("Successful Question creation!");
-    }
-  );
+    alert("Successful Question creation!");
+  });
 
   const handleModulesSelect = (selectedModules) => {
     setSelectedModules(selectedModules);
@@ -60,17 +56,11 @@ const PostQuestionPage = () => {
 
   let statusComponent = null;
   if (error !== null) {
-    statusComponent = (
-      <div id="postQuestion-status-component">
-        Error while trying to post question: {error.toString()}
-      </div>
-    );
+    statusComponent = <div id="postQuestion-status-component">Error while trying to post question: {error.toString()}</div>;
   }
 
   if (isLoading) {
-    statusComponent = (
-      <div id="postQuestion-status-component">Posting question...</div>
-    );
+    statusComponent = <div id="postQuestion-status-component">Posting question...</div>;
   }
 
   return (
@@ -85,27 +75,19 @@ const PostQuestionPage = () => {
         </span>
         !
       </h2>
-      <h3>
-        Got any bugs? Share your experience and let&apos;s find a solution
-        together.
-      </h3>
+      <h3>Got any bugs? Share your experience and let&apos;s find a solution together.</h3>
       <form id="postQuestionForm" onSubmit={handleSubmit}>
         <div id="postQuestion-input-area">
           <label htmlFor="question-title">
             What would you like to ask about?
-            <Input
-              name="question-title"
-              value={title}
-              onChange={(value) => setTitle(value)}
-              placeholder="Please enter your question title here."
-            />
+            <input name="question-title" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Please enter your question title here." />
           </label>
           <label htmlFor="question-content">
             Please share extra information and context about your question.
             <textarea
               name="question-content"
               value={content}
-              onChange={(event) => setContent(event.target.value)}
+              onChange={(e) => setContent(e.target.value)}
               placeholder="Please enter your question content here."
             ></textarea>
           </label>
